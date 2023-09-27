9d97f83adf2bb544cc9922d7f3b6cd50a4bc9b84
AlaaSultan <alaasultan3217@gmail.com>
2023-09-27 17:53:27 +0200

    add lab

---
+++ tests/modules/cpoe/order-entry/e2e-test/fav-list/addlab.page.ts
@@ -0,0 +1,47 @@
+import { Page,Locator } from "@playwright/test";
+export class addlab{
+
+    readonly page:Page;
+    readonly orders:Locator;
+    readonly orderentry:Locator;
+    readonly productclassificationfiled:Locator;
+    readonly laboratory:Locator;
+    readonly searchfield:Locator;
+    readonly selectedprouct:Locator;
+    readonly physicainsearch:Locator;
+    readonly selectedphysicain:Locator;
+    readonly orderbtn:Locator;
+
+
+    constructor(page:Page){
+        this.page=page;
+        this.orders=page.getByText('Orders', { exact: true });
+        this.orderentry=page.getByText('Order Entry');
+        this.productclassificationfiled=page.getByLabel('select').nth(2);
+        this.laboratory=page.('option', { name: 'Laboratory' }).locator('span');
+        this.searchfield=page.getByPlaceholder('Add new laboratory using the investigation code or name');
+        this.selectedprouct=page.locator('#ClassificationGridId td .ClassificationItem');
+        this.physicainsearch=page.getByPlaceholder('Requesting Physician *');
+        this.selectedphysicain=page.getByRole('option', { name: 'Abdel Ghafar Ahmed Selim' });
+        this.orderbtn=page.getByRole('button', { name: 'Order', exact: true });
+
+    }
+
+    async  Navigate_To_cpoe(url:string) {
+        await this.page.goto(url);
+        
+    }
+    async addlab(searchfield:string,selectedphysicain:string){
+        await this.orders.click();
+        await this.orderentry.click();
+        await this.productclassificationfiled.click;
+        await this.laboratory.click();
+        await this.searchfield.click();
+        await this.searchfield.fill('cbc');
+        await this.selectedprouct.hover();
+        await this.selectedprouct.click();
+        await this.physicainsearch.click();
+        await this.physicainsearch.fill('ahmed');
+        await this.selectedphysicain.click();
+        await this.orderbtn.click();
+    }};
